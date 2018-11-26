79652524201fffc16688d229ba20ca8d36b7b897
gqhao <haoguoqing123@sina.com>
2018-11-27 06:18:53 +0800

    fix the bug of empty conf file

--- plugin/htp_audit/htp_audit_main.cc
+++ plugin/htp_audit/htp_audit_main.cc
@@ -282,6 +282,14 @@ static int htp_audit_init_env_from_config(config_t *config)
   htp_audit_log_file[0] = 0;
   htp_audit_error_log_file[0] = 0;
 
+  if (config->group_amount ==0)
+  {
+    strcpy(htp_audit_log_file,"htp_audit.log");
+    strcpy(htp_audit_error_log_file,"htp_audit_error.log");
+    enable_buffer = 1;
+    return (0);
+  }
+
   group = config->groups;
   while (group != NULL)
   {
@@ -325,6 +333,8 @@ static int htp_audit_read_config_and_init_env()
 
   ret = htp_audit_init_env_from_config(config);
 
+  if (config->group_amount == 0)
+    return (0);
   config_destroy(config);
 
   return (ret);
